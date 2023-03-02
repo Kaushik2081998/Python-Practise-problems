ca2f1b9c897961edf739fda001d2f5c703d08182
kaushik <kau2081998@gmal.com>
2023-03-02 13:21:42 +0000

    First commit

---
+++ class.py
@@ -0,0 +1,27 @@
+class S1:
+
+    def __init__ (self):
+
+        self.list = ['India','UK','England','USA','Africa']
+
+        if len(self.list) == 5:
+            print("The length of list is 5")
+
+        elif len(self.list) <5 :
+            print("The length of list is 3 to 5")
+
+        else:
+            print("The length of the list is below 3")
+
+    def sample(self):
+        print("All is well")
+
+k1 = S1()
+k1.sample()
+
+
+
+
+
+
+
